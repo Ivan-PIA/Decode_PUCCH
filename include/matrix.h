394b0fdd80302185ed5f14f0b62501638168d26a
Ivan-PIA <wind.vanya@gmail.com>
2025-02-04 01:08:05 +0700

    add matrix G

---
+++ include/matrix.h
@@ -0,0 +1,16 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+#include <stdint.h>
+
+#define ROW  20  
+#define COL  13  
+
+#define RATE 10 
+
+
+
+extern int8_t G[ROW][COL];
+
+#endif
